56670d05970ad476aab83e7f1e8875e7424659ec
oliver-wilson-dev <contact.oliver.wilson@gmail.com>
2019-07-07 22:36:12 +0100

    feat: setup script

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
-# setup-mac
-Installs some programs that someone working in the web platform will likely need
+# Settings
+Get my developer setup the way that I like it on a new mac.
+
+Installs `git`, `node`, `yarn`, `npm`, `homebrew`, `google chrome`, `firefox`, `charles`, `copyq`, `docker`, `github for desktop`, `insomnia`, `iterm2`, `postman`, `slack`, `spotify`, `whatsapp`, `zeplin`.
+
+Sets `zsh` as the default shell for your machine.
+
+Installs `Visual Studio Code` with some useful extensions, the `DankMono` font, and some opinionated settings.
+
+## Prerequisites
+* You will need an asos github account with access to all of the repos in `asosteam`. Contact Dom Green or Tony Gorman if you don't already have access to these repositories in github.
+
+* You will be offered the option to download some of the most popular asos repos, if you wish to do this then you need to [create an access token](https://help.github.com/en/enterprise/2.17/user/articles/creating-a-personal-access-token-for-the-command-line) and use that as your password to install the repos.
+
+* Don't leave your mac as you will be required to enter your password a few times and will be asked to enter/configure git credentials.
+
+## Manual steps before installation
+* Download [this gist](https://gist.github.com/asos-oliverwilson/692c8faeed167fe6e16443d6ef2ee40d). 
+
+* Open the terminal and go to the directory where you saved [the gist](https://gist.github.com/asos-oliverwilson/692c8faeed167fe6e16443d6ef2ee40d) and run `sh setup.sh`.
+  * You might need to extract the file if you've downloaded it as a zip.
+
+## Manual steps after installation
+Disclaimer: you should have some settings files downloaded into the `~/mac-setup-files` directory.
+
+### iTerm2
+* Go to `Preferences -> Profiles -> Colors -> Color Presets -> Import...` and select `~/mac-setup-files/iterm2/iterm2-color-preset.itermcolors`. Then select the `iterm2-color-preset` option from the presets.
+
+* Go to `Preferences -> Profiles -> Text -> Font -> Change Font` and select a font that has _for Powerline_ in the name. Change your font size here too if you wish.
+
+* Go to `Preferences -> Profiles -> Keys -> Load Preset...` and select _Natural Text Editing_ - you can now jump around the text using the alt+arrow keys.
+
+### Reboot your mac
+* `Press the apple button in the top left of the screen -> Reboot...`
+
+## Some useful links
+* [Download virtualbox](https://www.virtualbox.org/wiki/Downloads) - the cask requires manual intervention, which is distracting from the rest of the setup script.
+* [Microsoft VM download for virtualbox](https://developer.microsoft.com/en-us/microsoft-edge/tools/vms/) - this takes quite a while to download.
+* [Forward network traffic from mac to VM](https://medium.com/@urubuz/accessing-localhost-in-mac-from-windows-vm-in-virtualbox-312a3de6fedb)
+* [Useful iTerm2 keybindings](https://medium.com/@_simon__says/useful-iterm2-keybindings-d9e4e9b8a3ed)
---
+++ setup.sh
@@ -0,0 +1,186 @@
+#!/bin/sh
+
+# check if something is already installed
+install_if_does_not_exist()
+{
+	if command -v "$1" >/dev/null 2>&1; then
+  		echo $1 'is already installed'
+	else
+		echo "installing" $1
+		brew install $1
+	fi
+}
+
+# Homebrew
+if test ! $(which brew); then
+    echo "Installing homebrew..."
+    yes '' | /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+brew tap caskroom/cask
+brew tap caskroom/versions
+
+# Install Git and set username and email
+install_if_does_not_exist git
+GIT_USERNAME="$(git config --global --get user.name)"
+GIT_EMAIL="$(git config --global --get user.email)"
+
+read -t 60 -p "Your git user.name is "${GIT_USERNAME}". Would you like to change it? [y/n]" -n 1 -r SET_GIT_USERNAME_REPLY
+echo
+if [[ $SET_GIT_USERNAME_REPLY =~ ^[Yy]$ ]]
+then
+	echo "Define your Git username"
+	read username
+	git config --global user.name "$username"
+	GIT_USERNAME="$(git config --global --get user.name)"
+fi
+
+read -t 60 -p "Your git user.email is "${GIT_EMAIL}". Would you like to change it? [y/n]" -n 1 -r SET_GIT_EMAIL_REPLY
+echo
+if [[ $SET_GIT_EMAIL_REPLY =~ ^[Yy]$ ]]
+then
+	echo "Define your Git email"
+	read email
+	git config --global user.email "$email"
+	GIT_EMAIL="$(git config --global --get user.email)"
+fi
+
+echo "Thanks. Your git user.name is '${GIT_USERNAME}' and your git user.email is '${GIT_EMAIL}'".
+
+# Clone setup files
+rm -rf ~/mac-setup-files
+git clone https://github.com/oliver-wilson-dev/settings.git ~/mac-setup-files
+git config --global core.editor "code --wait"
+
+
+# Install yarn
+install_if_does_not_exist yarn
+
+
+# NodeJS / NPM
+install_if_does_not_exist node
+npm list -g jest && echo "jest is installed globally" || echo "installing jest globally" && npm install -g jest
+npm list -g eslint && echo "eslint is installed globally" || echo "installing eslint globally" && npm install -g eslint
+
+
+# nvm (node version manager)
+chown -R $(whoami) /usr/local/lib/pkgconfig
+chmod u+w /usr/local/lib/pkgconfig
+mkdir ~/.nvm
+install_if_does_not_exist nvm
+
+
+# Visual Studio Code
+brew cask install visual-studio-code
+# install extensions
+	code --install-extension andrew-codes.cypress-snippets
+	code --install-extension bierner.markdown-preview-github-styles
+	code --install-extension christian-kohler.npm-intellisense
+	code --install-extension christian-kohler.path-intellisense
+	code --install-extension dbaeumer.vscode-eslint
+	code --install-extension eamidui.gitlens
+	code --install-extension esbenp.prettier-vscode
+	code --install-extension formukahendry.auto-close-tag
+	code --install-extension GitHub.vscode-pull-request-github
+	code --install-extension ngryman.codesandbox-theme
+	code --install-extension Orta.vscode-jest
+	code --install-extension PKief.material-icon-theme
+	code --install-extension shinnn.stylelint
+	code --install-extension shyykoserhiy.vscode-spotify
+	code --install-extension streetsidesoftware.code-spell-checker
+	code --install-extension Tyriar.sort-lines
+	code --install-extension xabikos.JavaScriptSnippets
+# copy settings over
+	yes | cp ~/mac-setup-files/vscode-settings.json ~/Library/Application\ Support/Code/User/settings.json
+
+
+# Install Programs
+
+# Browsers
+	BROWSERS=(
+		google-chrome
+		firefox
+	)
+	brew cask install ${BROWSERS[@]}
+
+# Other programs
+	OTHER_PROGRAMS=(
+		charles
+		copyq
+		docker
+		github
+		insomnia
+		iterm2
+		postman
+		slack 
+		spotify
+		whatsapp
+		zeplin
+	)
+
+	brew cask install ${OTHER_PROGRAMS[@]}
+
+# VirtualBox
+	brew cask install virtualbox
+	# could actually fetch the ie11 image and install it here too...
+
+# Mac settings
+defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
+defaults write com.apple.menuextra.battery ShowPercent -string "YES"
+defaults write com.apple.LaunchServices LSQuarantine -bool false
+defaults write NSGlobalDomain AppleShowAllExtensions -bool false
+yes | cp -a ~/mac-setup-files/DankMono/otf/. ~/Library/Fonts
+
+# Make asos directory and clone repos into it
+read -t 60 -p "Would you like to download some of the asos repos? [y/n]" -n 1 -r CLONE_ASOS_REPOS_REPLY
+echo
+if [[ $CLONE_ASOS_REPOS_REPLY =~ ^[Yy]$ ]]
+then
+	mkdir ~/asos
+	cd ~/asos
+	mkdir ./sitechrome
+	mkdir ./saved-lists
+	echo "Enter your git username"
+	read username
+	echo "Enter your git password (note, if you are trying to clone a repo that requires an access token, enter that instead.)"
+	read password
+	git clone https://$username:$password@github.com/asosteam/asos-web-my-account.git my-account
+	git clone https://$username:$password@github.com/asosteam/asos-web-bag.git bag
+	git clone https://$username:$password@github.com/asosteam/asos-web-checkout.git checkout
+	git clone https://$username:$password@github.com/asosteam/asos-web-product-listing-page.git plp
+	git clone https://$username:$password@github.com/asosteam/asos-web-productpage.git pdp
+	git clone https://$username:$password@github.com/asosteam/asos-web-saved-lists.git ./saved-lists/application
+	git clone https://$username:$password@github.com/asosteam/asos-web-saved-lists-stubs.git ./saved-lists/stubs
+	git clone https://$username:$password@github.com/asosteam/asos-web-site-chrome-client-node.git ./sitechrome/client
+	git clone https://$username:$password@github.com/asosteam/asos-web-site-chrome-publisher.git ./sitechrome/publisher
+fi
+
+# zsh
+install_if_does_not_exist zsh
+install_if_does_not_exist zsh-completions
+# install oh-my-zsh
+RUNZSH=no CHSH=no sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+# install powerline fonts
+	# clone
+	git clone https://github.com/powerline/fonts.git --depth=1
+	# install
+	cd fonts
+	./install.sh
+	# clean-up a bit
+	cd ..
+	rm -rf fonts
+# install powerlevel9k theme
+	if git clone --depth=1 https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k; then :
+	else
+		cd ~/.oh-my-zsh/custom/themes/powerlevel9k && git pull
+	fi
+
+# install zsh plugins
+	if git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/plugins/zsh-autosuggestions; then :
+	else
+		cd ~/.oh-my-zsh/plugins/zsh-autosuggestions && git pull
+	fi
+# copy zsh settings over
+	yes | cp ~/mac-setup-files/.zshrc ~/.zshrc
+
+chsh -s "$(command -v zsh)" "${USER}"
